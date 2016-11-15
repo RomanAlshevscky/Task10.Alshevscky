0e710a9936c370ff214ee4404f14c53adbc061b9
OSmol <Olga_Smolyakova@outlook.com>
2016-11-15 21:17:06 +0300

    Update PagePaths.java

--- src/by/epam/training/constants/PagePaths.java
+++ src/by/epam/training/constants/PagePaths.java
@@ -1,6 +1,6 @@
 package by.epam.training.constants;
 
-public class PagePaths {
+public class PagePaths {// что за класс, именованные во множественном числе, а еще предендущий на роль целого слоя
 	public final static String PAGE_ATTR = "PAGE_NAME";
 	public final static String MAIN_PAGE = "/WEB-INF/jsp/main.jsp";
 	public final static String LOGIN_PAGE = "/WEB-INF/jsp/signIn.jsp";
